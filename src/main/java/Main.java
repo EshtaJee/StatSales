8d818543ef07fa5c7807e40f6f9f2e6acb64dd6a
EshtaJee <Logrus-10@yandex.ru>
2022-07-21 22:45:34 +0300

    YoStat2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Main {
+import ru.eshtajee.statist.services.StatsService;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        StatsService service = new StatsService();
+
+        int[] allSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int sumSales = service.getSumSales(allSales);
+        int middleSales = service.getMiddleSales(allSales);
+        int picSalesMonth = service.getPicSalesMonth(allSales);
+        int minSalesMonth = service.getMinSalesMonth(allSales);
+        int monthDown = service.getMonthDown(allSales);
+        int monthHigh = service.getMonthHigh(allSales);
+
+        System.out.println();
+        System.out.println("Сумма продаж за период = "+ sumSales);
+
+        System.out.println("Средняя сумма продаж в месяц = "+ middleSales);
+
+        System.out.println("Номер месяца, в котором был пик продаж - "+ (picSalesMonth + 1));
+
+        System.out.println("Номер месяца, в котором был минимум продаж - "+ (minSalesMonth + 1));
+
+        System.out.println("Кол-во месяцев, в которых продажи были ниже среднего = "+ monthDown);
+
+        System.out.println("Кол-во месяцев, в которых продажи были выше среднего = "+ monthHigh);
+
+    }
 }
--- src/main/java/ru/eshtajee/statist/services/StatsService.java
+++ src/main/java/ru/eshtajee/statist/services/StatsService.java
@@ -1,2 +1,65 @@
-package PACKAGE_NAME;public class StatsService {
+package ru.eshtajee.statist.services;
+
+public class StatsService {
+
+    public int getSumSales(int[] allSales) {
+        int sumSales = 0; // Сумма всех продаж
+
+        for (int i = 0; i < allSales.length; i++) {
+            sumSales = sumSales + allSales[i];
+        }
+        return sumSales;
+    }
+
+    public int getMiddleSales(int[] allSales) {
+        int sumSales = getSumSales(allSales); // Сумма всех продаж
+        int middleSales = 0; // Средняя сумма продаж в месяц
+
+        for (int i = 0; i < allSales.length; i++) {
+            middleSales = sumSales / allSales.length;
+        }
+        return middleSales;
+    }
+
+    public int getPicSalesMonth(int[] allSales) {
+        int picSalesMonth = 0; // Номер месяца, в котором был пик продаж (осуществлены продажи на максимальную сумму)
+
+        for (int i = 0; i < allSales.length; i++) {
+            if (allSales[i] >= allSales[picSalesMonth])
+                picSalesMonth = i;
+        }
+        return picSalesMonth;
+
+    }
+
+    public int getMinSalesMonth(int[] allSales) {
+        int minSalesMonth = 0; // Номер месяца, в котором был минимум продаж (осуществлены продажи на минимальную сумму)
+
+        for (int i = 0; i < allSales.length; i++) {
+            if (allSales[i] <= allSales[minSalesMonth])
+                minSalesMonth = i;
+        }
+        return minSalesMonth;
+    }
+
+    public int getMonthDown(int[] allSales) {
+        int monthDown = 0; // Кол-во месяцев, в которых продажи были ниже среднего
+
+        for (int i = 0;  i < allSales.length; i++) {
+            if (allSales[i] < getMiddleSales(allSales))
+                monthDown = monthDown + 1;
+        }
+        return monthDown;
+    }
+
+    public int getMonthHigh(int[] allSales) {
+        int monthHigh = 0; // Кол-во месяцев, в которых продажи были выше среднего
+
+        for (int allSale : allSales) {
+            if (getMiddleSales(allSales) < allSale)
+                monthHigh = monthHigh + 1;
+        }
+        return monthHigh;
+    }
 }
+
